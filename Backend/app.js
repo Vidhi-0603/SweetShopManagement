304f377a96fa8660e69c766138aca1c14109995e
Vidhilika Gupta <gvidhilika0606@gmail.com>
2025-12-14 13:26:33 +0530

    Updated CORS origin to use environment variable; adjust SameSite cookie attribute based on production environment

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ app.use(cookieparser());
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
--- Backend/src/config/cookie.config.js
+++ Backend/src/config/cookie.config.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const cookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite:"lax",
+  sameSite: isProduction ? "none" : "lax",
   maxAge: 1000 * 60 * 60,
 };
 
